c72b31b106a089592a094c1312bf8e22bbe6a496
Mateusz Wieczorek <mateusz_wieczorek@epam.com>
2025-09-17 14:46:16 +0200

    Adding script to upload data to s3 using boto3

--- data/transform_txt_to_parquet.py
+++ data/transform_txt_to_parquet.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import os
 
-for file in os.listdir("./txt_format/"):
-    df = pd.read_csv(f"./txt_format/{file}")
-    new_name = file.split(".")[0] + ".parquet"
-    print(f"New parquet file = {new_name}")
-    df.to_parquet(f"./parquet_format/{new_name}")
+input_folder = "./txt_format/"
+output_folder = "./parquet_format/"
+
+os.makedirs(output_folder, exist_ok=True)
+
+for file in os.listdir(input_folder):
+    if file.endswith(".txt"):
+        df = pd.read_csv(os.path.join(input_folder, file), sep="\t", header=0)
+        new_name = file.split(".")[0] + ".parquet"
+        print(f"New parquet file = {new_name}")
+        df.to_parquet(os.path.join(output_folder, new_name), engine='pyarrow', index=False)
+
+# Check if we can read parquet file
+df = pd.read_parquet(os.path.join(output_folder, "dane_ankiet.parquet"))
+print(df)
---
+++ data/upload_data_to_s3.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Creating Session With Boto3.
+session = boto3.Session(aws_access_key_id=os.getenv("ACCESS_KEY"),
+                        aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"))
+
+# Creating S3 Resource From the Session.
+s3 = session.resource('s3')
+bucket_name = os.getenv("BUCKET_NAME")
+file_path = "./parquet_format"
+
+for file in os.listdir(file_path):
+    if file.endswith(".parquet"):
+        local_file = f"{file_path}/{file}"
+        s3_file_key = file
+
+        try:
+            s3.Bucket(bucket_name).upload_file(local_file, s3_file_key)
+            print(f"File: {file} upload succesfully!")
+        except Exception as e:
+            print(f"Expcetion occured!: {e}")
