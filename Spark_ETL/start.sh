5401eacb6ef64313fb9bb8dea0efb3bf04633cbe
Mateusz Wieczorek <mateusz_wieczorek@epam.com>
2025-09-24 13:06:12 +0200

    Restructuring src project and creating container for preparing .jar files

--- Spark_ETL/start.sh
+++ Spark_ETL/start.sh
@@ -3,20 +3,6 @@
 HADOOP_AWS_VERSION=3.3.2
 AWS_SDK_VERSION=1.12.262
 
-HADOOP_AWS_URL="https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/${HADOOP_AWS_VERSION}/hadoop-aws-${HADOOP_AWS_VERSION}.jar"
-AWS_SDK_URL="https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/${AWS_SDK_VERSION}/aws-java-sdk-bundle-${AWS_SDK_VERSION}.jar"
-
-# Download jars if they don't exist
-if [ ! -f ./jar_files/hadoop-aws-${HADOOP_AWS_VERSION}.jar ]; then
-    echo "Downloading hadoop-aws..."
-    curl -L -o hadoop-aws-${HADOOP_AWS_VERSION}.jar $HADOOP_AWS_URL
-fi
-
-if [ ! -f ./jar_files/aws-java-sdk-bundle-${AWS_SDK_VERSION}.jar ]; then
-    echo "Downloading aws-java-sdk-bundle..."
-    curl -L -o aws-java-sdk-bundle-${AWS_SDK_VERSION}.jar $AWS_SDK_URL
-fi
-
 # Run Spark with local jars
 echo "Starting Spark application..."
-time spark-submit --jars ./jar_files/hadoop-aws-${HADOOP_AWS_VERSION}.jar,./jar_files/aws-java-sdk-bundle-${AWS_SDK_VERSION}.jar ./app/main.py
+time spark-submit --jars ./jar_files/hadoop-aws-${HADOOP_AWS_VERSION}.jar,./jar_files/aws-java-sdk-bundle-${AWS_SDK_VERSION}.jar ./src/app/main.py
